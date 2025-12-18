e95fb678c923633a0facdaa2248c50ca4dee43fc
aikan86 <giuliopolent@gmail.com>
2025-12-18 10:34:06 +0100

    Update ScrollToTop.js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,35 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import ChiSono from './pages/ChiSono';
-import Psicomotricita from './pages/Psicomotricita';
-import Servizi from './pages/Servizi';
-import Contatti from './pages/Contatti';
-import './styles/App.css';
-import ScrollToTop from './components/ScrollToTop';
-function App() {
-  return (
-    <Router>
-      <ScrollToTop /> 
-      
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/chi-sono" element={<ChiSono />} />
-            <Route path="/psicomotricita" element={<Psicomotricita />} />
-            <Route path="/servizi" element={<Servizi />} />
-            <Route path="/contatti" element={<Contatti />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
 
-export default App;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
