b8cc7031ed29aee56fba13b1b180a0d452346856
aikan86 <32703516+aikan86@users.noreply.github.com>
2025-12-21 21:19:38 +0100

    google search

--- src/pages/Contatti copy.js
+++ src/pages/Contatti copy.js
@@ -248,6 +248,22 @@ function Contatti() {
         </div>
       </section>
 
+      <section className="map-section">
+        <h2>Dove trovarmi</h2>
+        <div className="map-container">
+          {/* Qui andrebbe inserito il codice per una mappa interattiva, ad esempio con Google Maps */}
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.654246112644!2d12.489838!3d41.902783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDU0JzEwLjAiTiAxMsKwMjknMjMuNCJF!5e0!3m2!1sit!2sit!4v1650000000000!5m2!1sit!2sit" 
+            width="100%" 
+            height="450" 
+            style={{ border: 0 }} 
+            allowFullScreen="" 
+            loading="lazy" 
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Mappa dello studio"
+          ></iframe>
+        </div>
+      </section>
     </div>
   );
 }
